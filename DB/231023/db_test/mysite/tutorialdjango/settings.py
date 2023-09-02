3ec35dd4dee660ea303e9d2668e575100316028a
ggengmo <rudah365dlf@gmail.com>
2023-09-02 13:00:00 +0900

    231023

--- DB/231023/db_test/mysite/tutorialdjango/settings.py
+++ DB/231023/db_test/mysite/tutorialdjango/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = "tutorialdjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# postgres:1234
+# username:password
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorialdjango',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
